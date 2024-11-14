9cebdca7bd3b76f8e132083e29ad880cb330312c
Giovani Carvalho <giovaniartil@gmail.com>
2024-11-14 14:01:53 -0400

    adaptação dos métodos para utilização das bibliotecas criadas

--- campo_minado_giovani_artil_oliveira_de_carvalho.c
+++ campo_minado_giovani_artil_oliveira_de_carvalho.c
@@ -1,95 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
+#include "matriz.h"
+#include "log.h"
 
 #define F 10
 #define M 20
 #define D 30
+#define bombF 15
+#define bombM 60
+#define bombD 135
 
-int **alocaMatriz(int tam){
-    int **mat = (int **)malloc(tam * sizeof(int *));
-
-    for (int i = 0; i < tam; i++){
-        mat[i] = (int *)calloc(tam * tam, sizeof(int)); // inicializa as matrizes do jogo com 0's
+/*Define o tamanho do campo e a quantidade de bombas com base na dificuldade escolhida*/
+void defineTamCampoQtdBombas(int dificuldade, int *qtdBombas, int *tamCampo){
+    if (dificuldade == 1){
+        *qtdBombas = bombF;
+        *tamCampo = F;
     }
+    else if (dificuldade == 2){
+        *qtdBombas = bombM;
+        *tamCampo = M;
+    }
+    else{
+        *qtdBombas = bombD;
+        *tamCampo = D;
+    }
+}
+
+/*Aloca uma matriz Nx2, que guardará as coordenadas das bombas, sendo N a quantidade de bombas e cada coluna representando uma coordenada*/
+int **alocaMatrizCoordenadasResposta(int qtdBombas){
+    int **mat = alocaMatriz(qtdBombas, 2);
 
     return mat;
 }
 
-int **alocaMatrizCoordenadasResposta(int tam, int *qtdBombas){ // aloca uma matriz com N linhas, onde N é a quantidade de bombas, e 2 colunas, em que cada linha representa a coordenada de uma bomba
-    int **mat;
-
-    if (tam == F){
-        *qtdBombas = 3;
-        mat = (int **)malloc(*qtdBombas * sizeof(int *));
-        for (int i = 0; i < *qtdBombas; i++){
-            mat[i] = (int *)malloc(2 * sizeof(int));
-        }
-    }
-    else if (tam == M){
-        *qtdBombas = 6;
-        mat = (int **)malloc(*qtdBombas * sizeof(int *));
-        for (int i = 0; i < *qtdBombas; i++){
-            mat[i] = (int *)malloc(2 * sizeof(int));
-        }
-    }
-    else{
-        *qtdBombas = 9;
-        mat = (int **)malloc(*qtdBombas * sizeof(int *));
-        for (int i = 0; i < *qtdBombas; i++){
-            mat[i] = (int *)malloc(2 * sizeof(int));
-        }
-    }
-
-    return mat;
-}
-
-void imprimeMatriz(int **mat, int tam, int flagModificadorChar){ // flagModificadorChar: indica se a impressão vai usar %c ou %d // 1 = %c, 0 = %d
-    printf("  ");
-    for (int i = 1; i <= tam; i++){
-        printf(" %2d", i);
-    }
-    printf("\n");
-
-    if (flagModificadorChar){
-        for (int i = 0; i < tam; i++){
-            printf("%2d ", i + 1);
-
-            for (int j = 0; j < tam; j++){
-                printf(" %c ", mat[i][j]);
-            }
-            printf("\n");
-        }
-    }
-    else{
-        for (int i = 0; i < tam; i++){
-            printf("%2d ", i + 1);
-            for (int j = 0; j < tam; j++){
-                printf("%2d ", mat[i][j]);
-            }
-            printf("\n");
-        }
-    }
-}
-
-int defineTamCampo(char *dificuldade){
-    int tamCampo;
-
-    if (!strcmp(dificuldade, "facil")){
-        tamCampo = F;
-    }
-    else if (!strcmp(dificuldade, "medio")){
-        tamCampo = M;
-    }
-    else{
-        tamCampo = D;
-    }
-
-    return tamCampo;
-}
-
-void gerarPosicaoBombas(int **mat, int qtdBombas, int tam){ // gera coordenadas aleatórias para as bombas
+/*Gera a posição das bombas de maneira aleatória e usando como seed o tempo decorrido desde 01/01/1970*/
+void gerarPosicaoBombas(int **mat, int qtdBombas, int tam){
     int x, y, contBombasGeradas = 0;
 
     while (contBombasGeradas < qtdBombas){
@@ -100,20 +46,20 @@ void gerarPosicaoBombas(int **mat, int qtdBombas, int tam){ // gera coordenadas 
 
         // verificação se a coordenada gerada é igual à alguma coordenada já armazenada
         if (!contBombasGeradas){ // verifica se é a primeira coordenda gerada
-            mat[contBombasGeradas][0] = x;
-            mat[contBombasGeradas][1] = y;
+            insereMatriz(mat, contBombasGeradas, 0, x);
+            insereMatriz(mat, contBombasGeradas, 1, y);
             contBombasGeradas++;
         }
         else{
             for (int i = 0; i < contBombasGeradas; i++){
                 if (mat[i][0] == x && mat[i][1] == y){
-                    flagCoordenadaRepetida = 1;
+                    flagCoordenadaRepetida = 1; // caso a coordenada gerada seja repetida, o contador não irá ser incrementado
                 }
             }
 
             if (!flagCoordenadaRepetida){
-                mat[contBombasGeradas][0] = x;
-                mat[contBombasGeradas][1] = y;
+                insereMatriz(mat, contBombasGeradas, 0, x);
+                insereMatriz(mat, contBombasGeradas, 1, y);
                 contBombasGeradas++;
             }
         }
@@ -177,33 +123,77 @@ int verificaQuantidadeBombas(int **mat, int x, int y, int tam){ // verifica quan
 void preencheMatrizJogo(int **mat, int tam){ // preenche a matriz com 120, código do 'x' na tabela ASCII
     for (int i = 0; i < tam; i++){
         for (int j = 0; j < tam; j++){
-            mat[i][j] = 120;
+            insereMatriz(mat, i, j, 120);
         }
     }
 }
 
+// Preenche a matriz resposta colocando as bombas em suas coordenadas e quantas bombas tem ao redor de cada coordenada
 void preencheMatrizResposta(int **mat, int **coordResp, int tam, int qtdBomba){
     for (int i = 0; i < qtdBomba; i++){ // preenche os lugares das bombas no campo
-        mat[coordResp[i][0]][coordResp[i][1]] = -1;
+        insereMatriz(mat, coordResp[i][0], coordResp[i][1], -1);
     }
 
     for (int i = 0; i < tam; i++){ // preenche o restante dos lugares do campo
         for (int j = 0; j < tam; j++){
             if (mat[i][j] != -1){
-                mat[i][j] = verificaQuantidadeBombas(mat, i, j, tam);
+                insereMatriz(mat, i, j, verificaQuantidadeBombas(mat, i, j, tam));
             }
         }
     }
 }
 
+/*O método vai revelar a casa que o usuário digitou. Caso ela seja igual a 0, ele vai revelar as casas ao redor, se elas também forem iguais a 0, o método continuará revelando
+as casas até que ele ache uma casa diferente 0*/
+void revelaCasa(int **jogo, int **resposta, int x, int y, int tam){
+    if(!resposta[x][y]){
+        insereMatriz(jogo, x, y, resposta[x][y]+48);
+
+        if(x + 1 < tam && jogo[x+1][y] == 120){
+            revelaCasa(jogo, resposta, x+1, y, tam);
+        }
+
+        if(x - 1 >= 0 && jogo[x-1][y] == 120){
+            revelaCasa(jogo, resposta, x-1, y, tam);
+        }
+
+        if(y + 1 < tam && jogo[x][y+1] == 120){
+            revelaCasa(jogo, resposta, x, y+1, tam);
+        }
+
+        if(y - 1 >= 0 && jogo[x][y-1] == 120){
+            revelaCasa(jogo, resposta, x, y-1, tam);
+        }
+
+        if(x-1 >= 0 && y-1 >= 0 && jogo[x-1][y-1] == 120){
+            revelaCasa(jogo, resposta, x-1, y-1, tam);
+        }
+
+        if(x+1 < tam && y-1 >= 0 && jogo[x+1][y-1] == 120){
+            revelaCasa(jogo, resposta, x+1, y-1, tam);
+        }
+
+        if(x-1 >= 0 && y+1 < tam && jogo[x-1][y+1] == 120){
+            revelaCasa(jogo, resposta, x-1, y+1, tam);
+        }
+
+        if(x+1 < tam && y+1 < tam && jogo[x+1][y+1] == 120){
+            revelaCasa(jogo, resposta, x+1, y+1, tam);
+        }
+    }else if(resposta[x][y] != -1){
+        insereMatriz(jogo, x, y, resposta[x][y]+48);
+    }
+}
+
+
 int verificaJogada(int **jogo, int **resposta, int x, int y, int tam){
     int resultado;
 
     if((0 <= x && x < tam) && (0 <= y && y < tam)){ // verifica se as coordenadas estão dentro do limite do tamanho
-        if (jogo[x][y] != resposta[x][y]+48){ // verifica se é uma jogada repetida
+        if (jogo[x][y] != resposta[x][y]+48){ // verifica se é uma jogada repetida, comparando se a casa escolhida já foi revelada e possui mesmo valor que casa da matriz resposta
             if (resposta[x][y] != -1){
-                jogo[x][y] = resposta[x][y] + 48; // transforma o número de bombas em código ASCII
-                resultado = 1;                    // jogador não achou uma bomba, jogo continua
+                revelaCasa(jogo, resposta, x, y, tam); // transforma o número de bombas em código ASCII
+                resultado = 1; // jogador não achou uma bomba, jogo continua
             }
             else{
                 resultado = 0; // jogador achou uma bomba e perdeu o jogo
@@ -221,8 +211,28 @@ int verificaJogada(int **jogo, int **resposta, int x, int y, int tam){
     return resultado;
 }
 
+/*Por conta de agora várias casas poderem serem reveladas, esse método vai sempre verificar quantas casas não foram reveladas e subtrair pela quantidade de bombas para ter 
+a quantidade de jogadas restantes*/
+void verificaQuantidadeJogadas(int **jogo, int tam, int *qtdJogadas, int qtdBombas){
+    *qtdJogadas = 0;
+    for(int i = 0; i < tam; i++){
+        for(int j = 0; j < tam; j++){
+            if(jogo[i][j] == 120){
+                *qtdJogadas = *qtdJogadas + 1;
+            }
+        }
+    }
+    
+    *qtdJogadas = *qtdJogadas - qtdBombas;
+}
+
 void inicioJogo(int **campoJogo, int **campoResposta, int **coordenadasResposta, int tamCampo, int qtdBombas){
     int qtdJogadas = tamCampo * tamCampo - qtdBombas, bombaExplodiu = 0;
+
+    // cria o arquivo que conterá as jogadas do usuário junto da data e hora que o jogo começou
+    FILE *log = criaArquivo();
+    imprimeDataHora(log);
+    fechaLog(log);
 
     gerarPosicaoBombas(coordenadasResposta, qtdBombas, tamCampo);
 
@@ -233,6 +243,7 @@ void inicioJogo(int **campoJogo, int **campoResposta, int **coordenadasResposta,
         int x, y;
         printf("\nDigite uma coordenada: \n");
         scanf("%d,%d", &x, &y);
+
         // colocar as coordenadas digitadas para as equivalentes em C, Ex: se o usuário digita "1,1", vira 0,0
         x--;
         y--;
@@ -242,7 +253,7 @@ void inicioJogo(int **campoJogo, int **campoResposta, int **coordenadasResposta,
         if (jogada != -1){
             if (jogada){
                 imprimeMatriz(campoJogo, tamCampo, 1);
-                qtdJogadas--;
+                verificaQuantidadeJogadas(campoJogo, tamCampo, &qtdJogadas, qtdBombas);
             }
             else{
                 bombaExplodiu = 1;
@@ -250,6 +261,11 @@ void inicioJogo(int **campoJogo, int **campoResposta, int **coordenadasResposta,
         }else{
             printf("JOGADA INVALIDADA! Essa coordenada ja foi digitada ou uma coordenada invalida foi passada.\n");
         }
+
+        // apoós cada jogada, é feito o registro no log
+        abreArquivo(log);
+        imprimeLog(log, campoJogo, tamCampo, x+1, y+1, 1);
+        fechaLog(log);
     }
 
     if (!bombaExplodiu){
@@ -260,37 +276,30 @@ void inicioJogo(int **campoJogo, int **campoResposta, int **coordenadasResposta,
     }
 
     imprimeMatriz(campoResposta, tamCampo, 0);
+    abreArquivo(log);
+    imprimeLog(log, campoResposta, tamCampo, 0, 0, 0);
+    
+    fechaLog(log);
 }
 
 int main(void){
     srand((unsigned int)time(NULL)); // seed do gerador aleatório, baseado no tempo
-    char *dificuldade = malloc(8 * sizeof(char));
-    int **campoJogo, **campoResposta, **coordenadasResposta, tamCampo, qtdBombas;
+    int **campoJogo, **campoResposta, **coordenadasResposta, tamCampo, qtdBombas, dificuldade;
 
-    printf("Digite a dificuldade:\nfacil\nmedio\ndificil\n\n- ");
-    scanf("%s", dificuldade);
+    printf("Digite a dificuldade:\n1 - facil\n2 - medio\n3 - dificil\n\n- ");
+    scanf("%d", &dificuldade);
 
-    tamCampo = defineTamCampo(dificuldade);
-    campoResposta = alocaMatriz(tamCampo);
-    campoJogo = alocaMatriz(tamCampo);
-    coordenadasResposta = alocaMatrizCoordenadasResposta(tamCampo, &qtdBombas);
+    defineTamCampoQtdBombas(dificuldade, &qtdBombas, &tamCampo);
+    campoResposta = alocaMatrizQuadrada(tamCampo);
+    campoJogo = alocaMatrizQuadrada(tamCampo);
+    coordenadasResposta = alocaMatrizCoordenadasResposta(qtdBombas);
 
     inicioJogo(campoJogo, campoResposta, coordenadasResposta, tamCampo, qtdBombas); // método que inicia o jogo
 
-    // liberando todos os espaços alocados das matrizes e dos vetores
-    for (int i = tamCampo; i < tamCampo; i++){
-        free(campoJogo[i]);
-        free(campoResposta[i]);
-    }
-    free(campoJogo);
-    free(campoResposta);
-
-    for (int i = 0; i < qtdBombas; i++){
-        free(coordenadasResposta[i]);
-    }
-    free(coordenadasResposta);
-
-    free(dificuldade);
+    // liberação de memória alocada
+    liberaMatriz(campoJogo, tamCampo);
+    liberaMatriz(campoResposta, tamCampo);
+    liberaMatriz(coordenadasResposta, qtdBombas);
 
     return 0;
 }
